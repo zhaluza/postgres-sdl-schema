853c45d41c3d0d6d8c596acb3e168bc30ccc269a
Zac H <zac.haluza@gmail.com>
2020-04-09 19:56:32 -0400

    added refactored queries

--- server/programmaticSchema/schema.js
+++ server/programmaticSchema/schema.js
@@ -2,11 +2,13 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema,
   GraphQLNonNull,
 } = require('graphql');
+const db = require('../dbConnect');
 
-// User Type
+// users table
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
@@ -17,18 +19,91 @@ const UserType = new GraphQLObjectType({
   }),
 });
 
+// posts table
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: { type: GraphQLString },
+    text: { type: GraphQLString },
+    author: {
+      type: UserType,
+      resolve(parent, args) {
+        try {
+          const query = `SELECT * FROM posts
+        WHERE author=$1`;
+          const values = [parent.id];
+          return db.query(query, values).then((res) => res.rows[0]);
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    },
+  }),
+});
+
 // Root Query
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
-  user: {
-    type: UserType,
-    args: {
-      id: { type: GraphQLString },
+  fields: {
+    // GET all users
+    users: {
+      type: new GraphQLList(UserType),
+      resolve() {
+        try {
+          const query = `SELECT * FROM users`;
+          return db.query(query).then((res) => res.rows);
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
     },
-    resolve(parent, args) {
-      console.log(parent);
+    // GET single user by id
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        try {
+          const query = `SELECT * FROM users
+          WHERE id = $1`;
+          const values = [args.id];
+          return db.query(query, values).then((res) => res.rows[0]);
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    },
+    // GET all posts
+    posts: {
+      type: PostType,
+      resolve() {
+        try {
+          const query = `SELECT * FROM posts`;
+          return db.query(query).then((res) => res.rows);
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    },
+    // GET single post by id
+    post: {
+      type: PostType,
+      resolve(parent, args) {
+        try {
+          const query = `SELECT * FROM posts
+          WHERE id = $1`;
+          const values = [args.id];
+          return db.query(query, values).then((res) => res.rows[0]);
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
     },
   },
 });
 
-module.exports = new GraphQLSchema({});
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
